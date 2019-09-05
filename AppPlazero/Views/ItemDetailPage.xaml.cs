799376f88849714e0d11a88ec11e340cedf7c6d1
david.campos <david.campos@thomasgreg.com>
2019-09-05 13:59:25 -0500

    Agrega Picker Unidad Medida

--- AppPlazero/Views/ItemDetailPage.xaml.cs
+++ AppPlazero/Views/ItemDetailPage.xaml.cs
@@ -6,18 +6,32 @@ namespace AppPlazero.Views
 {
     public partial class ItemDetailPage : ContentPage
     {
+        Producto ProductoItem;
         public ItemDetailPage()
         {
             InitializeComponent();
-            BindingContext = new Producto();
+            ProductoItem = new Producto();
+            //ProductoItem = (Producto)BindingContext;
+            BindingContext = ProductoItem; // new Producto();
         }
 
         async void OnGuardarClicked(object sender, EventArgs e)
         {
-            var ProductoItem = (Producto)BindingContext;
             await App.TodoManager.SaveTaskAsync(ProductoItem, true);
             await Navigation.PopAsync();
 
+        }
+
+        void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                ProductoItem.pro_nidUnidadBase = selectedIndex;
+                //monkeyNameLabel.Text = picker.Items[selectedIndex];
+            }
         }
     }
 }
