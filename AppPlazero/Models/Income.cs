42976bb4b3ce9f854092eaa7262d54df964ee24f
Ing David Pad <daalejito20@gmail.com>
2019-10-14 00:06:47 -0500

    Carga de datos ingreso de mercancia

--- AppPlazero/Models/Income.cs
+++ AppPlazero/Models/Income.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
-using J = Newtonsoft.Json.JsonPropertyAttribute;
+using Newtonsoft.Json;
 
 namespace AppPlazero.Models
 {
-    public class Income
+    public class Income 
     {
-        [J("ing_nidIngreso")] private int IdIngreso;
-        [J("Nombre")] public string Nombre;
-        [J("Fecha")] public DateTime Fecha;
-        [J("Total")] public double Total;
-        [J("ing_sRefDocumento")] public string refDocumento;    
+
+        [JsonProperty("ing_nidIngreso")]
+        private Int32 IdIngreso; 
+
+        [JsonProperty("Nombre")]
+        public string Nombre { get; set; }
+
+        [JsonProperty("ing_fFechaIngreso")]
+        public DateTime FechaIngreso { get; set; }
+
+        [JsonProperty("ing_fCostoTotal")]
+        public  float CostoTotal { get; set; }
+
+        [JsonProperty("ing_sRefDocumento")]
+        public string Documento { get; set; }
+
+        //[{"ing_nidIngreso":"1","Nombre":"DAVID CAMPOS","Fecha":"2019-10-11 11:47:29",
+        //    "Total":"155500","ing_sRefDocumento":"Recibo de caja"}]
+
     }
 }
